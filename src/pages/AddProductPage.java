894637b53e41153468967cdcf80c16f9751bcb1c
Abdelrhman Mohamed <61846392+abdelrhmanmo@users.noreply.github.com>
2021-09-25 21:58:21 +0200

    Update AddProductPage.java

--- src/pages/AddProductPage.java
+++ src/pages/AddProductPage.java
@@ -143,6 +143,9 @@ public class AddProductPage extends JPanel{
                     msg.setText("Check Your Data Again Please!!!");
                 }
             }
+
+
+
         });
 
         backBtn.addActionListener((ActionEvent ae)->{
@@ -196,57 +199,72 @@ public class AddProductPage extends JPanel{
         frame.setVisible(true);
         frame.setResizable(false);
     }
-    private void typingRules(){
+    private void typingRules() {
 
-        insertBtn.addKeyListener(new KeyAdapter() {
+        sellingPrice.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 jTextFieldKeyTyped(e);
                 if (e.getKeyChar() == KeyEvent.VK_ENTER ) {
-                    if(productName.getText().isEmpty() || sellingPrice.getText().isEmpty() || buyingPrice.getText().isEmpty() || quantity.getText().isEmpty()) {
-
-                        msg.setBounds(210,370,200,20);
-                        msg.setText("Fill All Fields !");
-                    }else {
-                        try {
-                            double d1 = Double.parseDouble(sellingPrice.getText());
-                            double d2 = Double.parseDouble(buyingPrice.getText());
-                            int i = Integer.parseInt(quantity.getText());
-                            if (d1 < 0){
-                                msg.setBounds(175, 370, 330, 20);
-                                msg.setText("You Enter Selling Price with Negative Value");
-                            }
-                            else if (d2 < 0){
-                                msg.setBounds(175, 370, 330, 20);
-                                msg.setText("You Enter Buying Price with Negative Value");
-                            }
-                            else if (i < 0){
-                                msg.setBounds(175, 370, 330, 20);
-                                msg.setText("You Enter Product Quantity with Negative Value");
-                            }
-                            else if(i==0){
-                                msg.setBounds(175, 370, 330, 20);
-                                msg.setText("You Enter Product Quantity :  0");
-                            }
-                            else {
-                                ConfirmPopUp pop = new ConfirmPopUp(frame,productName.getText());
-                                if (pop.getResult() == 0) {
-                                    msg.setBounds(210, 370, 200, 20);
-                                    msg.setText(insertProcess());
-                                } else {
-                                    dateLblValue.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
-                                }
-                            }
-                        }
-                        catch (NumberFormatException nfe){
-                            msg.setBounds(210, 370, 210, 20);
-                            msg.setText("Check Your Data Again Please!!!");
-                        }
-                    }
+                    insertBtnChecking();
                 }
             }
         });
+        buyingPrice.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                jTextFieldKeyTyped(e);
+                if (e.getKeyChar() == KeyEvent.VK_ENTER ) {
+                    insertBtnChecking();
+                }
+            }
+
+        });
+        quantity.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                jTextFieldKeyTyped(e);
+                if (e.getKeyChar() == KeyEvent.VK_ENTER ) {
+                    insertBtnChecking();
+                }
+            }
+        });
+
+        
     }
+
+    private void insertBtnChecking(){
+        try {
+            double d1 = Double.parseDouble(sellingPrice.getText());
+            double d2 = Double.parseDouble(buyingPrice.getText());
+            int i = Integer.parseInt(quantity.getText());
+            if (d1 < 0) {
+                msg.setBounds(175, 370, 330, 20);
+                msg.setText("You Enter Selling Price with Negative Value");
+            } else if (d2 < 0) {
+                msg.setBounds(175, 370, 330, 20);
+                msg.setText("You Enter Buying Price with Negative Value");
+            } else if (i < 0) {
+                msg.setBounds(175, 370, 330, 20);
+                msg.setText("You Enter Product Quantity with Negative Value");
+            } else if (i == 0) {
+                msg.setBounds(175, 370, 330, 20);
+                msg.setText("You Enter Product Quantity :  0");
+            } else {
+                ConfirmPopUp pop = new ConfirmPopUp(frame, productName.getText());
+                if (pop.getResult() == 0) {
+                    msg.setBounds(210, 370, 200, 20);
+                    msg.setText(insertProcess());
+                } else {
+                    dateLblValue.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+                }
+            }
+        } catch (NumberFormatException nfe) {
+            msg.setBounds(210, 370, 210, 20);
+            msg.setText("Check Your Data Again Please!!!");
+        }
+    }
+
     private void jTextFieldKeyTyped(java.awt.event.KeyEvent evt){
         char c = evt.getKeyChar();
         if(!(Character.isDigit(c) || (c == KeyEvent.VK_BACK_SPACE) || c == KeyEvent.VK_DELETE)){
@@ -269,5 +287,4 @@ public class AddProductPage extends JPanel{
         }
         return "Select A Category";
     }
-
 }
